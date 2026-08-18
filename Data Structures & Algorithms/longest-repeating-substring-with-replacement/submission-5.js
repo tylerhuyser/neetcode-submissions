81f6c7378dbf7d43072c7adf7247bfb9f9145d15
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2026-08-18 14:13:48 -0400

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let frequencies = {}
+        let mostFrequent = 0
+        let maxLength = 0
+
+        let left = 0
+        let right = 0
+
+        while (right < s.length) {
+            frequencies[s[right]] = (frequencies[s[right]] || 0) + 1
+            mostFrequent = Math.max(mostFrequent, frequencies[s[right]])
+            if (right - left + 1 - mostFrequent <= k) {
+                maxLength = Math.max(maxLength, right - left + 1)
+            } else {
+                frequencies[s[left]]--
+                left++
+            }
+
+            right++
+        }
+
+        return maxLength
+    }
+}
