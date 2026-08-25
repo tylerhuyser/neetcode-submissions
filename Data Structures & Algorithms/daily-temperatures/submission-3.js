354095daabbf8cb838db6884bf1e613699393c74
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2026-08-25 16:22:29 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        // 1. Create a stack and an array to return as a result.
+        let stack = []
+        let result = new Array(temperatures.length).fill(0)
+
+        // 2. Iterate through the temperatures.
+        for (let i = 0; i < temperatures.length; i++) {
+            let currentTemp = temperatures[i]
+            // 2a. While there is a stack, and currentTemp is greater than previous temps, push the difference as a result.
+            while (stack.length > 0 && currentTemp > temperatures[stack[stack.length - 1]]) {
+                let popped = stack.pop()
+                result[popped] = i - popped
+            }
+            // 3. Always push the index of the current Temp.
+            stack.push(i)
+        }
+
+        return result
+    }
+}
