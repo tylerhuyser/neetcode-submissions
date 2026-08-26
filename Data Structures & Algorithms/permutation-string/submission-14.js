523fd0ced192ac3b7d86970811eec46c65c15dbc
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2026-08-26 13:39:12 -0400

    Add: permutation-string - submission-14

---
+++ Data Structures & Algorithms/permutation-string/submission-14.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        let dict = {}
+        for (const char of s1) {
+            dict[char] = (dict[char] || 0) + 1
+        }
+
+        for (let  i = 0; i < s2.length; i++) {
+            let currentChar = s2[i]
+            let tempDict = {...dict}
+
+            if (currentChar in tempDict) {
+                tempDict[currentChar]--
+
+                for (let j = i + 1; j < s2.length; j++) {
+                    let nextChar = s2[j]
+                    if (!tempDict[nextChar] || tempDict[nextChar] === 0) break
+                    else tempDict[nextChar]--
+                }
+
+            } else {
+                continue
+            }
+
+            let values = Object.values(tempDict)
+            if (values.every(value => value === 0)) return true
+        }
+
+
+        return false
+        
+    }
+}
