33194c0786451de5238d7f99ad8276e38489c1a0
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2026-08-27 13:36:16 -0400

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let stack = []
+        let result = new Array(temperatures.length).fill(0)
+
+        for (let i = 0; i < temperatures.length; i++) {
+            let currentTemp = temperatures[i]
+
+            while (stack.length > 0 && currentTemp > temperatures[stack[stack.length - 1]]) {
+                let popped = stack.pop()
+                result[popped] = i - popped
+            }
+
+            stack.push(i)
+        }
+
+        
+        return result
+    }
+}
